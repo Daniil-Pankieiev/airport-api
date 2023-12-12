35761a0cd4992ad9b21c2819fba79ab77c618935
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-12 14:43:43 +0200

    Added JWTAuthentication and and made an email field for authorization

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -133,3 +133,9 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
