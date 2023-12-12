ef843b19e41d6572066230901f0cd26379b27b5e
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-12 12:48:05 +0200

    Added filters and pagination

--- airport/views.py
+++ airport/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
-from .models import Airport, Route, Crew, Ticket, Order, Airplane, AirplaneType, Flight
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Airport, Route, Crew, Order, Airplane, AirplaneType, Flight
 from .serializers import (
     AirportSerializer,
     RouteSerializer,
     CrewSerializer,
-    TicketSerializer,
     OrderSerializer,
     AirplaneTypeSerializer,
     AirplaneSerializer,
@@ -22,6 +23,16 @@ from .serializers import (
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        closest_big_city = self.request.query_params.get("city")
+
+        if closest_big_city:
+            queryset = queryset.filter(closest_big_city__icontains=closest_big_city)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return AirportListSerializer
@@ -40,6 +51,7 @@ class RouteViewSet(viewsets.ModelViewSet):
             return RouteDetailSerializer
         return RouteSerializer
 
+
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
@@ -53,15 +65,31 @@ class AirplaneTypeViewSet(viewsets.ModelViewSet):
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+
     def get_serializer_class(self):
         if self.action == "list":
             return AirplaneListSerializer
 
         return AirplaneSerializer
 
+
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        source_city = self.request.query_params.get("source_city")
+        destination_city = self.request.query_params.get("destination_city")
+        if source_city:
+            queryset = queryset.filter(
+                route__source__closest_big_city__icontains=source_city
+            )
+        if destination_city:
+            queryset = queryset.filter(
+                route__destination__closest_big_city__icontains=destination_city
+            )
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -69,10 +97,15 @@ class FlightViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return FlightDetailSerializer
         return FlightSerializer
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 10
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    pagination_class = OrderPagination
 
     def get_queryset(self):
         queryset = self.queryset.filter(user=self.request.user.id)
