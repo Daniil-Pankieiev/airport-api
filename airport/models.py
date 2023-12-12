cc183d9c7b8d028ab553f0d4fd20b16c532595c1
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-12 14:22:22 +0200

    Image field for airplane

--- airport/models.py
+++ airport/models.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-
-
+import os
+import uuid
+from django.utils.text import slugify
 from airport_api.settings import AUTH_USER_MODEL
 
 
@@ -49,8 +50,16 @@ class Route(models.Model):
         return f"{self.source.closest_big_city} - {self.destination.closest_big_city}"
 
 
+def airplane_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(null=True, upload_to=airplane_image_file_path)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(
